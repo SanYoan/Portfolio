6bf330636fb0174b482d07c5611fad57ab88a9f6
Yoan San <nasub76@gmail.com>
2024-10-11 17:13:14 +0200

    Updates

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -46,13 +46,9 @@ $(document).ready(function () {
     var anchor = $(this);
     $("html, body")
       .stop()
-      .animate(
-        {
-          scrollTop: $(anchor.attr("href")).offset().top - 0,
-        },
-        1200,
-        "easeInOutExpo"
-      );
+      .animate({
+        scrollTop: $(anchor.attr("href")).offset().top - 0,
+      });
   });
 
   $("body").scrollspy({
